f2fe4158087733f745bd9ef5299f47899c94f4fd
thangvm <vuthang.hustit@gmail.com>
2023-02-28 02:27:18 +0700

    add crawler pancake

--- src/main/java/com/fastwok/crawler/job/CrawlerFwSchedule.java
+++ src/main/java/com/fastwok/crawler/job/CrawlerFwSchedule.java
@@ -1,6 +1,7 @@
 package com.fastwok.crawler.job;
 
 import com.fastwok.crawler.services.isservice.TaskOrderService;
+import com.fastwok.crawler.services.isservice.TaskPancakeOrderService;
 import com.fastwok.crawler.services.isservice.TaskPushItemPancakeService;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +14,10 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class CrawlerFwSchedule {
     @Autowired
-    TaskPushItemPancakeService taskPushItemPancakeService;
+    TaskPancakeOrderService taskPancakeOrderService;
 
-    @Scheduled(fixedDelay = 600000)
+    @Scheduled(fixedDelay = 60000)
     public void importData2() throws UnirestException, InterruptedException {
-        taskPushItemPancakeService.getData();
+        taskPancakeOrderService.getData();
     }
 }
